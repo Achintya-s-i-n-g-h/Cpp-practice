e56b55a4b2225b4d880b85f9b69e43b74021e61e
Achintya-s-i-n-g-h <99878269+Achintya-s-i-n-g-h@users.noreply.github.com>
2022-07-11 16:46:08 +0530

    Add files via upload

---
+++ macros.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define vl vector<ll>
+#define pb push_back
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vii vector<pii>
+#define vll vector<pll>
+#define vb vector<bool>
+#define vc vector<char>
+#define vs vector<string>
+#define vvi vector<vi>
+#define vvl vector<vl>
+
+#define all(v) (v).begin(), (v).end()
+#define rep(i, a, b) for (ll i = a; i < b; i++)
+#define rrep(i, a, b) for (ll i = b; i >= a; i--)
+#define calc_fact(n) tgamma(n + 1)
+
+#define endl "\n"
+#define print(v)       \
+    for (auto &it : v) \
+        cout << it << " ";
+#define si(x) scanf("%d", &x)
+#define sll(x) scanf("%lld", &x)
+#define ss(s) scanf("%s", s)
+#define deb(x) cout << #x << "=" << x << endl
+
+#define MOD 1000000007
+
+/*
+    1. Reverse thought process
+    2. Handle corner cases
+    3. Check boundary conditions (array bounds)
+    4. Don't get stuck on one approach
+*/
+
+void solution()
+{
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+        solution();
+
+    return 0;
+}
